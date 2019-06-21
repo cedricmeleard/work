5abf87406598092bc7a4dca21e6c1b38da788ac9
Cédric Méléard <cedric.meleard@gmail.com>
2019-06-21 11:01:15 +0200

    TCR @ 0

--- dojos/TCR/fizzbuzz.spec.js
+++ dojos/TCR/fizzbuzz.spec.js
@@ -1,6 +1,6 @@
 const fizzbuzz = require("./fizzbuzz");
 
 test("when given 0 to fizzbiuzz should return 0", () => {
-    expect(fizzbuzz(0))
-        .toEqual("0");
+  expect(fizzbuzz(0))
+    .toEqual("1");
 })
